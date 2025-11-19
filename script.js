709c9bbcea755f13c2eee63fa50bc230bc57adb0
LentellASU <blentell@email.asu.edu>
2025-11-19 16:14:03 -0500

    First commit, home page complete, case study 1 page in progress

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add active class to clicked link
+document.querySelectorAll(".nav-link").forEach((link) => {
+	link.addEventListener("click", function () {
+		document
+			.querySelectorAll(".nav-link")
+			.forEach((l) => l.classList.remove("active"));
+		this.classList.add("active");
+	});
+});
