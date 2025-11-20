33dbe4eacd832999c1ee649979817970f431647e
LentellASU <blentell@email.asu.edu>
2025-11-20 12:12:01 -0500

    Case studies pages added, styled, mobile responsive added

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ document.querySelectorAll(".nav-link").forEach((link) => {
 		this.classList.add("active");
 	});
 });
+// Hamburger Menu Toggle
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("header nav");
+
+hamburger.addEventListener("click", function () {
+    this.classList.toggle("active");
+    nav.classList.toggle("active");
+});
+
+// Close menu when clicking a nav link
+document.querySelectorAll(".nav-link").forEach((link) => {
+    link.addEventListener("click", function () {
+        hamburger.classList.remove("active");
+        nav.classList.remove("active");
+    });
+});
